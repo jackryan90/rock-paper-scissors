b86cead522d6008f62542460a2aab29f13ddb936
Jack Webster <jack.ryan.webster@gmail.com>
2025-09-12 00:29:47 -0600

    Refactor to let player choose with buttons
    Previously, the user had to type in his choice, and the program played
    exactly five rounds. The "Revisiting Rock Paper Scissors" lesson in
    The Odin Project instructed me to insert three buttons to let the user
    make his choice. Upon the press of any button, the program simulates
    a round. The game ends when either the player or computer reaches five
    points.

--- js/game.js
+++ js/game.js
@@ -1,3 +1,23 @@
+function printGreeting(output) {
+  const greeting = document.createTextNode("Let's play rock-paper-scissors!");
+  output.appendChild(greeting);
+  output.appendChild(document.createElement("br"));
+  return;
+}
+
+function printRoundHeader() {
+  const lines = document.createTextNode(
+    "--------------------------------------------"
+  );
+  output.appendChild(lines);
+  output.appendChild(document.createElement("br"));
+
+  const roundHeader = document.createTextNode(`Round ${round}`);
+  output.appendChild(roundHeader);
+  output.appendChild(document.createElement("br"));
+  return;
+}
+
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
   switch (randomNumber) {
@@ -10,50 +30,39 @@ function getComputerChoice() {
   }
 }
 
-function getPlayerChoice() {
-  return prompt("Enter your choice: ").toLowerCase();
+function printPlayerChoices(computerChoice, playerChoice) {
+  const choices = document.createTextNode(
+    `You chose '${playerChoice}' and the computer chose '${computerChoice}'. `
+  );
+  output.appendChild(choices);
+  return;
 }
 
 function getRoundWinner(computerChoice, playerChoice) {
+  let winnerText;
+  let winner;
+
   if (computerChoice === playerChoice) {
-    console.log("This round was a tie.");
-    return "tie";
-  }
-  
-  if (
+    winnerText = document.createTextNode("It's a tie.");
+    winner = "tie";
+  } else if (
     ((computerChoice === "rock") && (playerChoice === "paper"))
     || ((computerChoice === "paper") && (playerChoice === "scissors"))
     || ((computerChoice === "scissors") && (playerChoice === "rock"))
   ) {
-    console.log(
-      "You win! "
-      + `${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)} `
-      + `beats ${computerChoice}.`
-    );
-    return "player";
+    winnerText = document.createTextNode("You win!");
+    winner = "player";
+  } else {
+    winnerText = document.createTextNode("You lose!");
+    winner = "computer";
   }
-  
-  console.log(
-    "You lose! "
-    + `${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} `
-    + `beats ${playerChoice}.`
-  );
-  return "computer";
+
+  output.appendChild(winnerText);
+  output.appendChild(document.createElement("br"));
+  return winner;
 }
 
-function printRoundHeader(round) {
-  console.log("--------------------------------------------");
-  console.log(`Round ${round}`);
-  return;
-}
-
-function printPlayerChoices(computerChoice, playerChoice) {
-  console.log(`The computer chose: ${computerChoice}`);
-  console.log(`You chose: ${playerChoice}`);
-  return;
-}
-
-function calculateScores(winner, scores) {
+function calculateScores(winner) {
   switch (winner) {
     case "player":
       scores.player++;
@@ -64,42 +73,70 @@ function calculateScores(winner, scores) {
     default:
       break;
   }
-  return;
 }
 
-function printFinalScores(scores) {
-  console.log("")
-  console.log("--------------------------------------------");
-  console.log("Final Score");
-  console.log(`You: ${scores.player}`);
-  console.log(`Computer: ${scores.computer}`);
-  
+function printCurrentScore() {
+  const scoreText = document.createTextNode(
+    `Current score: player: ${scores.player}, computer: ${scores.computer}`
+  );
+  output.appendChild(scoreText);
+  output.appendChild(document.createElement("br"));
+}
+
+function printWinner() {
+  const lines = document.createTextNode(
+    "--------------------------------------------"
+  );
+  output.appendChild(lines);
+  output.appendChild(document.createElement("br"));
+
+  let winnerText;
   if (scores.player === scores.computer) {
-    console.log("You tied!");
+    winnerText = document.createTextNode("You tied!");
   } else {
-    console.log(
+    winnerText = document.createTextNode(
       `${scores.player > scores.computer ? "You " : "The computer "} won!`
     );
   }
+  output.appendChild(winnerText);
   return;
 }
 
-function playGame() {
-  var scores = {"player": 0, "computer": 0}
+function playRound(playerChoice) {
+  if (!keepPlaying) return;
 
-  console.log("Let's play rock-paper-scissors!");
+  printRoundHeader();
 
-  for (var round = 1; round <= 5; round++) {
-    printRoundHeader(round);
+  const computerChoice = getComputerChoice();
+  printPlayerChoices(computerChoice, playerChoice);
 
-    const computerChoice = getComputerChoice();
-    const playerChoice = getPlayerChoice();
-    printPlayerChoices(computerChoice, playerChoice);
+  const winner = getRoundWinner(computerChoice, playerChoice);
+  calculateScores(winner);
+  printCurrentScore();
 
-    var winner = getRoundWinner(computerChoice, playerChoice);
-    calculateScores(winner, scores);
+  for (let key in scores) {
+    if (scores[key] >= playToScore) {
+      keepPlaying = false;
+      printWinner();
+      break;
+    }
   }
 
-  printFinalScores(scores);
-  return;
+  round++;
 }
+
+const output = document.querySelector("#output");
+printGreeting(output);
+
+let scores = {"player": 0, "computer": 0}
+
+let round = 1;
+const playToScore = 5;
+let keepPlaying = true;
+
+const buttons = document.querySelectorAll(".button-container > button");
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    playRound(button.id);
+  });
+});
